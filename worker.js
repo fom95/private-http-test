687298d7a22c822b837baa0004a991062f272c54
fom95 <freeoffermail95@gmail.com>
2026-09-14 18:45:29 -0400

    Update worker.js

--- worker.js
+++ worker.js
@@ -1,15 +1,29 @@
 export default {
     async fetch(request, env) {
+
+        const apiId =
+            await env.TELEGRAM_SECRETS
+                .get("API_ID");
+
+        const apiHash =
+            await env.TELEGRAM_SECRETS
+                .get("API_HASH");
+
+        const session =
+            await env.TELEGRAM_SECRETS
+                .get("TELEGRAM_SESSION");
+
         return new Response(
             JSON.stringify({
                 worker: true,
-                apiId: !!env.API_ID,
-                apiHash: !!env.API_HASH,
-                session: !!env.TELEGRAM_SESSION
+                apiId: !!apiId,
+                apiHash: !!apiHash,
+                session: !!session
             }),
             {
                 headers: {
-                    "Content-Type": "application/json"
+                    "Content-Type":
+                        "application/json"
                 }
             }
         );
